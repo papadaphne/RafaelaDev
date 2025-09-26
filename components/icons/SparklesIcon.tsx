4a490bb1dbab8c178c36a2433d4d372a7bf4110f
papadaphne <arinkumanja76@gmail.com>
2025-09-26 12:48:25 +0700

    Add files via upload

---
+++ components/icons/SparklesIcon.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+export const SparklesIcon: React.FC = () => (
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="20" 
+        height="20" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        strokeWidth="2" 
+        strokeLinecap="round" 
+        strokeLinejoin="round" 
+        className="lucide lucide-sparkles"
+    >
+        <path d="m12 3-1.9 1.9a10.4 10.4 0 0 0-3.4 2.4L2 12l5.7 4.7c1 1 2.3 1.5 3.4 1.5s2.4-.5 3.4-1.5L22 12l-4.7-4.7c-1-1-2.3-1.5-3.4-1.5s-2.4.5-3.4 1.5L12 3z" />
+        <path d="M5 3v4" />
+        <path d="M19 17v4" />
+        <path d="M3 5h4" />
+        <path d="M17 19h4" />
+    </svg>
+);
